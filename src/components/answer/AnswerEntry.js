174565ae464ffd3642daf6de31a3a54c6a7149e3
SEULGI YOON <sgyoon.info@gmail.com>
2020-01-18 18:25:09 +0900

    :pencil: Fix typo and remove not required codes

--- src/components/answer/AnswerEntry.js
+++ src/components/answer/AnswerEntry.js
@@ -21,7 +21,7 @@ class AnswerEntry extends Component {
       editedAnswerContents : {
         contents: ''
       },
-      havePermission: true,
+      havePermission: false,
       isEditable: false
     }
   }
@@ -49,11 +49,8 @@ class AnswerEntry extends Component {
     if (isLogin && questionFlag && username === answerContents.username) {
       this.setState({
         havePermission: true
-      });
+      }, () => console.log('답글 퍼미션 허가: ', this.state.havePermission));
     }
-    // questionFlag, answerUsername이 NULL임. 이유 : 실제 구현된 api요청과 그냥 state에 가짜 데이터를 넣어서 랜더되는 속도의 차이때문
-    console.log('isLogin', isLogin, 'username', username, 'questionFlag', questionFlag, 'answerUsername', answerContents.username)
-    console.log('답글 퍼미션 허가: ', this.state.havePermission); // 이거 false나옴
   }
 
   getAnswerContents = (id) => {
@@ -65,8 +62,8 @@ class AnswerEntry extends Component {
           editedAnswerContents : {
             contents : res.data.contents
           } 
-        });
-      }, () => this.handleHavePermission())
+        }, () => this.handleHavePermission());
+      })
       .catch(err => {
         console.log(err.message);
         // this.setState({ errorMessage: err.message });
@@ -74,19 +71,19 @@ class AnswerEntry extends Component {
   }
 
   modifyAnswer = () => {
-    const { answerContent } = this.state;
+    const { answerContents } = this.state;
     const { contents } = this.state.editedAnswerContents;
     const body = {};
-    if (answerContent.contents !== contents) {
+    if (answerContents.contents !== contents) {
       body['contents'] = contents;
     }
 
     if (Object.keys(body).length > 0) {
-      axios.patch(`http://localhost:5000/answer/${answerContent.id}`, body)
+      axios.patch(`http://localhost:5000/answer/${answerContents.id}`, body)
       .then(res => {
         console.log('답글 수정 성공');
         // 다시 해당 글 정보 요청
-        this.getAnswerContents(answerContent.id);
+        this.getAnswerContents(answerContents.id);
         // 답글 수정모드 취소
         this.toggleIsEditable();
       }).catch(err => {
@@ -98,16 +95,23 @@ class AnswerEntry extends Component {
     } 
   }
 
+  
+
   deleteAnswer = () => {
     const { id } = this.state.answerContents;
 
     axios.delete(`http://localhost:5000/answer/${id}`)
     .then(res => {
-      console.log('답글 삭제 성공')
+      console.log('답글 삭제 성공');
+      // this.props.handleIsRenderChild();
+      // this.props.getAnswerListInformation(this.props.askId);
       // 새로고침 -> 이거 안먹는다!
       // this.props.history.go(0);
-      // 해당 질문글 정보 다시 요청해야하나...???
-    }).catch(err => {
+      // 해당 질문글 정보 다시 요청해야하나...??? 
+      // -> 요청하면 나오긴 하는데, 기존에 랜더된 답글들이 지워지지 않고 고대로 있음.
+    })
+    .then(() => this.props.getAnswerListInformation(this.props.askId))
+    .catch(err => {
       console.log(err.message);
       // this.setState({ errorMessage: err.message });
     });
@@ -129,16 +133,16 @@ class AnswerEntry extends Component {
     const { modifyAnswer, deleteAnswer, handleInputChange, toggleIsEditable } = this;
     const { answerContents, havePermission, editedAnswerContents, isEditable } = this.state;
     return (
-      <AnswerTemplate
-        answerContents={answerContents}
-        editedAnswerContents={editedAnswerContents}
-        havePermission={havePermission}
-        isEditable={isEditable}
-        modifyAnswer={modifyAnswer}
-        deleteAnswer={deleteAnswer}
-        handleInputChange={handleInputChange}
-        toggleIsEditable={toggleIsEditable}
-      />
+        <AnswerTemplate
+          answerContents={answerContents}
+          editedAnswerContents={editedAnswerContents}
+          havePermission={havePermission}
+          isEditable={isEditable}
+          modifyAnswer={modifyAnswer}
+          deleteAnswer={deleteAnswer}
+          handleInputChange={handleInputChange}
+          toggleIsEditable={toggleIsEditable}
+        />
     );
   }
 }
