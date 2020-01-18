def1be84d9af46faa819716bfd0054366c5a2361
SEULGI YOON <sgyoon.info@gmail.com>
2020-01-18 22:05:45 +0900

    :fire: Remove not required codes and comments

--- src/components/answer/AnswerList.js
+++ src/components/answer/AnswerList.js
@@ -10,12 +10,8 @@ class AnswerList extends Component {
     super(props);
     this.state = {
       answers: [],
-      commentsCount: null,
-      didUpdate: false
     };
   }
-
-  // const { username, isLogin, askId,  questionFlag } = this.props;
   
   getAnswerListInformation = (id) => {
     axios.get(`http://localhost:5000/answers/${id}`)
@@ -31,59 +27,37 @@ class AnswerList extends Component {
       });
   }
 
-  // handleIsRenderChild = () => {
-  //   this.setState({
-  //     isRenderChild: false
-  //   });
-  // }
-
   componentDidMount() {
     console.log('AnswerList.js - componentDidMount 불림')
 
-    const { askId, commentsCount } = this.props;
-    this.setState({commentsCount: commentsCount});
+    const { askId } = this.props;
     this.getAnswerListInformation(askId);
   }
   
   componentDidUpdate(prevProps, prevState) {
     console.log('AnswerList.js  - componentDidUpdate 불림')
-    console.log('AnswerList.js  -', prevState, prevProps)
-    if(prevState.answers.length !== this.state.answers.length) {
-      this.setState({
-        didUpdate: true
-      });
-    }
-    // if(prevProps.commentsCount !== this.state.commentsCount) {
-    //   console.log('AnswerList.js  - componentDidUpdate 다시 목록 요청함')
-    //   this.getAnswerListInformation(prevProps.askId);
-    // } => 무한 요청함    
   }
 
   render() {
     const { getAnswerListInformation, handleIsRenderChild } = this;
-    const { answers, didUpdate } = this.state;
+    const { answers } = this.state;
     const { isLogin, username, questionFlag, askId } = this.props;
-    console.log('AnswerList.js 의 questionFlag', questionFlag);
-    if (didUpdate) {
-      return (
-        <div>
-          {answers.map(answer => 
-            <AnswerEntry 
-              key={answer.id}
-              id={answer.id}
-              askId={askId}
-              isLogin={isLogin}
-              username={username}
-              questionFlag={questionFlag}
-              handleIsRenderChild={handleIsRenderChild}
-              getAnswerListInformation={getAnswerListInformation}
-            />
-          )}
-        </div>
-      );
-    } else {
-      return null;
-    }
+    return (
+      <div>
+        {answers.map(answer => 
+          <AnswerEntry 
+            key={answer.id}
+            id={answer.id}
+            askId={askId}
+            isLogin={isLogin}
+            username={username}
+            questionFlag={questionFlag}
+            handleIsRenderChild={handleIsRenderChild}
+            getAnswerListInformation={getAnswerListInformation}
+          />
+        )}
+      </div>
+    );
   }
 }
 
