02283e5a8d307d69bbb41bff949255289b6242d5
SEULGI YOON <sgyoon.info@gmail.com>
2020-01-21 22:45:01 +0900

    :construction: Show 'Back to Post' button if user clicked username from ask post

--- src/pages/User.js
+++ src/pages/User.js
@@ -8,11 +8,14 @@ class User extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: null,
-      username: null,
-      postCount: null,
-      commentsCount: null,
-      createdAt: null
+      userInfo: {
+        id: null,
+        username: null,
+        postCount: null,
+        commentsCount: null,
+        createdAt: null
+      },
+      fromAskEntry: false
     }
   }
 
@@ -21,7 +24,7 @@ class User extends Component {
     .then(res => {
       console.log('유저 정보 요청 성공')
       this.setState({
-        ...res.data
+        userInfo: res.data
       });
       console.log(res.data);
     }).catch(err => {
@@ -33,10 +36,14 @@ class User extends Component {
   componentDidMount() {
     this.props.changeCurrentPage('user');
     
-    if (this.props.isLogin) {
+    // if (this.props.isLogin) {
       const username = this.props.match.params.username;
       this.getUserInfo(username);
-    }
+      if (this.props.currentPage === 'askEntry')
+      this.setState({
+        fromAskEntry: true
+      });
+    // }
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -46,11 +53,13 @@ class User extends Component {
     }
   }
   
-  
+  // 로그인 안 한 유저라도 다른사람의 유저정보 확인 가능하게 만들기
+  // 어떤 글에서 유저창으로 들어왔을 경우 뒤로가기 버튼 만들기
   render() {
     const { isLogin, match } = this.props;
-    const { username, createdAt, postCount, commentsCount } = this.state;
-    if (isLogin) {
+    const { fromAskEntry } = this.state;
+    const { username, createdAt, postCount, commentsCount } = this.state.userInfo;
+    // if (isLogin) {
       return (
         <div>
           {this.props.username === match.params.username ? <h3>내 정보</h3> : <h3>{username} 정보</h3>}
@@ -58,11 +67,12 @@ class User extends Component {
           <p>가입일: {createdAt}</p>
           {this.props.username === match.params.username && <p>작성한 질문글 수: {postCount}</p>}
           {this.props.username === match.params.username && <p>작성한 답글 수: {commentsCount}</p>}
+          {fromAskEntry && <button onClick={() => this.props.history.goBack()}>글로 돌아가기</button>}
         </div>
       );
-    } else {
-      return <Redirect to="/" />
-    }
+    // } else {
+    //   return <Redirect to="/" />
+    // }
   }
 }
 
