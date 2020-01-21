22322e3bc19e5bb4a0fbc593e33d3aca29ac3799
SEULGI YOON <sgyoon.info@gmail.com>
2020-01-21 16:28:11 +0900

    :bug: Fix bug - Show plain contents has no keyword in it

--- src/components/ask/AskListTemplate.js
+++ src/components/ask/AskListTemplate.js
@@ -10,12 +10,14 @@ const AskListTemplate = (props) => {
     padding: '15px'
   }
  
+  // 텍스트 안에 검색 키워드가 없다면 그대로 반환하고, 있다면 키워드를 <mark>태그로 감싸 텍스트에 넣어 반환한다.
   const markKeywords = (text) => {
     const regex = new RegExp(keyword, 'gi');
+    // 텍스트 안에 키워드가 안 들어있으면 그냥 생 텍스트를 반환한다.
     if (!text.match(regex)) return text;
     const markedText = text.replace(regex, `<mark>${keyword}</mark>`);
-
-    return markedText;
+    // <mark>로 감싸진 키워드가 포함된 문장을 반환
+    return markedText; 
   }
 
   const convertToHTMLElement = (text) => {
@@ -44,7 +46,10 @@ const AskListTemplate = (props) => {
       <p>작성일 : {createdAt}</p>
       <p>작성자 : {username}</p>
       <p>답변수 : {commentsCount}</p>
-      {keyword === '' ? <p>내용 : {cutContents(contents)}</p> : <p>내용검색 : {cutMarkedText(contents)}</p>}
+      {keyword === '' ? <p>내용 : {cutContents(contents)}</p> 
+        : markKeywords(contents) === contents ? <p>내용 : {cutContents(contents)}</p> 
+          : <p>내용검색 : {cutMarkedText(contents)}</p>
+      }
       {answers && answers.length > 0 && <p>답글검색 : {cutMarkedText(answers[0].contents)}</p>}
     </div>
   );
